4e09aeae2dc04629489506361ae91f417917ddc1
mailzahidul <mailzahidul@gmail.com>
2021-10-01 15:38:14 +0600

    product model create

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product, Category
 
 # Register your models here.
+
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'discount_price', 'category']
+    list_display_links = ['price']
+    list_editable = ['title', 'discount_price', 'category']
+
+admin.site.register(Product, ProductAdmin)
+
+
+admin.site.register(Category)
--- shop/models.py
+++ shop/models.py
@@ -20,3 +20,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+
+
