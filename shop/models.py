f2ca18518c3e9f38debea453bdcfdd75b6523db7
mailzahidul <mailzahidul@gmail.com>
2021-10-03 22:58:19 +0600

    image size

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from PIL import Image
 
 # Create your models here.
 
@@ -17,6 +18,15 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     description = models.TextField()
     image = models.ImageField(upload_to='product/')
+
+    def save(self):
+        super().save()                              # saving image first
+        img = Image.open(self.image.path)           # Open image using self
+        print(self.image.path, "path.........")
+        if img.height > 300 or img.width > 300:
+            new_img = (300, 300)
+            img.thumbnail(new_img)
+            img.save(self.image.path)               # saving image at the same path
 
     def __str__(self):
         return self.title
